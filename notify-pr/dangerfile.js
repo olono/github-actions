e58c99be0b01e484668f3900059ba44e416bd69d
Marcus Motill <marcus.motill@nexd.io>
2019-06-07 15:02:27 -0500

    chill

--- notify-pr/dangerfile.js
+++ notify-pr/dangerfile.js
@@ -2,16 +2,18 @@
 
 const { WebClient } = require('@slack/web-api');
 const _ = require('lodash');
-const event = require(process.env.GITHUB_EVENT_PATH)
+const event = require(process.env.GITHUB_EVENT_PATH);
 
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const token = process.env.SLACK_TOKEN;
 
-  // An access token (from your Slack app or custom integration - xoxp, xoxb)
-  const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
 
-  const web = new WebClient(token);
-
-  (async () => {
-      // See: https://api.slack.com/methods/chat.postMessage
-      // change channel to userid?
-      const res = await web.chat.postMessage({ channel: '#pr-mentions', text: `ay ${JSON.stringify({path: process.env.GITHUB_EVENT_PATH, event})}` });
-  })();
+(async () => {
+    // See: https://api.slack.com/methods/chat.postMessage
+    // change channel to userid?
+    const res = await web.chat.postMessage({
+        channel: '#pr-mentions',
+        text: `ay ${JSON.stringify({ path: process.env.GITHUB_EVENT_PATH, event: event.action })}`
+    });
+})();
