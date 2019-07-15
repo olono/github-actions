6b99f1f0c422ee3da6fce5e66db4594364e0a310
Scott Gress <scott.gress@olono.ai>
2019-07-14 22:38:30 -0500

    filter on context and status

--- process-check-suite/index.js
+++ process-check-suite/index.js
@@ -1,4 +1,31 @@
-const eventPath = process.env.GITHUB_EVENT_PATH;
+const _ = require('lodash');
 
+const ALLOWED_CONTEXTS = {
+    semaphoreci: 'Semaphore'
+};
+
+const eventPath = process.env.GITHUB_EVENT_PATH;
 const eventJson = require(eventPath);
+
+if (!ALLOWED_CONTEXTS[_.get(eventJson, 'context')]) {
+    process.exit(78);
+}
+
+const status = _.get(eventJson, 'status');
+if (status === 'pending') {
+    process.exit(78);
+}
+
+if (status === 'failed') {
+    console.log('FAIL!');
+}
+
+if (status === 'failure') {
+    console.log('FAIL!');
+}
+
+if (status === 'error') {
+    console.log('ERROR!');
+}
+
 console.dir({ env: process.env, json: eventJson }, { depth: null });
