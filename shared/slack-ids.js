2dd50bb407106094d54866e53f144b1b99a2c420
Scott Gress <scott.gress@olono.ai>
2019-07-15 13:05:18 -0500

    Those weren't the right channels
    Will just have to wait until they add the app!

--- shared/slack-ids.js
+++ shared/slack-ids.js
@@ -1,9 +1,9 @@
 module.exports = {
     sgress454: { id: 'U9KTERZ7Z', channel: 'DL759KA4R' },
     'dan-greff': { id: 'U04TEG8RM', channel: 'DLFKWHGKG' },
-    khouse: { id: 'U6KSFH24Q', channel: 'D9JA6Q68H' },
+    khouse: { id: 'U6KSFH24Q', channel: '' },
     craigforster: { id: 'U9ZE7DGTE', channel: 'DLEQ07EKA' },
-    mwchapel: { id: 'U3RKGLEG7', channel: 'D9KNG4GGM' },
+    mwchapel: { id: 'U3RKGLEG7', channel: '' },
     ryanspoone: { id: 'U93C9250Q', channel: 'DL2Q50MUJ' },
     davisonn: { id: 'U941FUMAQ', channel: 'DL3C1EZ5G' }
 };
