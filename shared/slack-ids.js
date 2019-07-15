4188ead38687f56099a7e42919f491d00cb41178
Scott Gress <scott.gress@olono.ai>
2019-07-15 12:22:14 -0500

    Update channel IDs

--- shared/slack-ids.js
+++ shared/slack-ids.js
@@ -1,12 +1,9 @@
 module.exports = {
     sgress454: { id: 'U9KTERZ7Z', channel: 'DL759KA4R' },
     'dan-greff': { id: 'U04TEG8RM', channel: 'DLFKWHGKG' },
-    khouse: { id: 'U6KSFH24Q', channel: '' },
-    craigforster: { id: 'U9ZE7DGTE', channel: '' },
-    mwchapel: { id: 'U3RKGLEG7', channel: '' },
+    khouse: { id: 'U6KSFH24Q', channel: 'D9JA6Q68H' },
+    craigforster: { id: 'U9ZE7DGTE', channel: 'DLEQ07EKA' },
+    mwchapel: { id: 'U3RKGLEG7', channel: 'D9KNG4GGM' },
     ryanspoone: { id: 'U93C9250Q', channel: 'DL2Q50MUJ' },
-    michaelhowe063: { id: 'U6M7F57D0', channel: '' },
-    davisonn: { id: 'U941FUMAQ', channel: '' },
-    rousselle: { id: 'U0AUBBJ1Z', channel: '' },
-    'j-m-davison': { id: 'U04SSSA4N', channel: '' }
+    davisonn: { id: 'U941FUMAQ', channel: 'DL3C1EZ5G' }
 };
